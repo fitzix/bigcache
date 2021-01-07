13925e84975c82d7f68122ed1aed245451b4c0e1
fitzix <caojunkaiv@gmail.com>
2021-01-07 10:51:43 +0800

    feat: x

---
+++ cache.go
@@ -0,0 +1,41 @@
+package bigcache
+
+import (
+	"time"
+
+	"github.com/allegro/bigcache/v2"
+)
+
+type Cache struct {
+	cache *bigcache.BigCache
+}
+
+func NewCache(config bigcache.Config) (*Cache, error) {
+	cache, err := bigcache.NewBigCache(config)
+	return &Cache{cache: cache}, err
+}
+
+func (c *Cache) Set(key string, value byte, TTL time.Duration) error {
+	timeBinary, err := time.Now().Add(TTL).MarshalBinary()
+	if err != nil {
+		return err
+	}
+	v := append(timeBinary, value)
+	return c.cache.Set(key, v)
+}
+
+func (c *Cache) Get(key string) ([]byte, error) {
+	value, err := c.cache.Get(key)
+	if err != nil {
+		return nil, err
+	}
+	var bestBefore time.Time
+	err = bestBefore.UnmarshalBinary(value[:15])
+	if err != nil {
+		return nil, err
+	}
+	if time.Now().After(bestBefore) {
+		return nil, bigcache.ErrEntryNotFound
+	}
+	return value, nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/fitzix/bigcache
+
+go 1.15
+
+require github.com/allegro/bigcache/v2 v2.2.5 // indirect
